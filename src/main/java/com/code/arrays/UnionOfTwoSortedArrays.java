a37b77198bfeb1b1288a0b060a6599906b34f8db
Monika Nishad <monikanishad@Monikas-MacBook-Air.local>
2023-09-12 20:15:42 +0530

    Added a Class that finds the Union Of Two Sorted Arrays

---
+++ src/main/java/com/code/arrays/UnionOfTwoSortedArrays.java
@@ -0,0 +1,41 @@
+package com.code.arrays;
+
+public class UnionOfTwoSortedArrays {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5, 7};
+        int[] arr2 = {2,4,6} ;           //Output: Union : {1, 2, 3, 4, 5, 6, 7} 
+        int i =0, j =0, k=0;
+
+        int[] arr3 = new int[arr1.length + arr2.length];
+
+        while(i < arr1.length && j < arr2.length){
+            if(arr1[i] < arr2[j]){
+                arr3[k] = arr1[i];
+                i++;
+            }else{
+                arr3[k] = arr2[j];
+                j++;
+            }
+            k++;
+        }
+
+        if(i < arr1.length){
+            for(;i < arr1.length ; i++){
+                arr3[k] = arr1[i];
+                k++;
+            }
+        }
+
+        if(j < arr2.length){
+            for( ; j< arr2.length; j++){
+                arr3[k] = arr2[j];
+                k++;
+            }
+        }
+        
+        for (int l = 0; l < arr3.length; l++) {
+            System.out.print(arr3[l] +" ");           
+        }
+    }
+       
+}
