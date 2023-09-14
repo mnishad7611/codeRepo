f0a5937abff0f25412963a50e5908592495713f1
Monika Nishad <monikanishad@Monikas-MacBook-Air.local>
2023-09-14 22:29:03 +0530

    Added a class that Reverse an Array

---
+++ src/main/java/com/code/arrays/ReverseArray.java
@@ -0,0 +1,63 @@
+package com.code.arrays;
+
+public class ReverseArray {
+    public static int[] reverseUsingExtraArray(int[] arr) {       
+        int n = arr.length;
+        int[] arr2 = new int[n];
+        int j =0;
+
+        for (int i = n-1; i >= 0 ; i--) {
+            //arr2[n-1-i] = arr[i];
+            arr2[j] = arr[i];
+            j++;            
+        }
+
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
+            
+        }
+        return arr;
+        
+    }
+
+    public static int[] reverseUsingTwoPointers(int[] arr){
+        int n = arr.length;
+        int i =0; int j = n-1; int temp =0;
+
+        while(i <= j){           
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++; j--;
+        }
+        for (int k = 0; k < arr.length; k++) {
+            System.out.println(arr[k]);
+        }
+        return arr;
+        
+    }
+
+    public static void reverseUsingForLoop(int[] arr){
+        int n = arr.length;
+        int temp =0;
+
+       for (int i = 0; i < n/2 ; i++) {
+            temp = arr[i];
+            arr[i] = arr[n-1-i];
+            arr[n-1-i] = temp;            
+       }      
+            
+        for (int k = 0; k < arr.length; k++) {
+            System.out.println(arr[k]);
+        }       
+
+    }
+
+    public static void main(String[] args) {
+        
+        int[] arr = {1,2,3,4,5};
+        reverseUsingTwoPointers(arr);
+        reverseUsingForLoop(arr);
+    }
+    
+}
